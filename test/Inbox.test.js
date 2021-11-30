ed4e672219d16a0b476b5be37adfe98b55d300e5
Cuong <bdcuong91@gmail.com>
2021-11-30 20:27:48 +0100

    Smart Contract Example

---
+++ test/Inbox.test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const {
+  abi,
+  evm
+} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+});
+
+describe('Inbox', () => {
+  it('should deploy a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('should have a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('should change the message', async () => {
+    await inbox.methods.setMessage('bye').send({
+      from: accounts[0]
+    });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+});
